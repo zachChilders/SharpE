35e1c460fa83a2b4863c611633821d4308c26118
zachChilders <zwc101@gmail.com>
2015-03-25 15:35:11 -0700

    Test Class

---
+++ MatrixTest/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharpe.Matrix;
+using Sharpe.Numbers;
+
+namespace MatrixTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Identity Matrix");
+            IdentityMatrix idMatrix = new IdentityMatrix(3);
+            Console.WriteLine(idMatrix.ToString());
+
+            Console.WriteLine("Creating 3x3 Double Matrix");
+            Matrix ZeroMatrix = new Matrix(3, 3);
+            ZeroMatrix[2][2] = 1.0;
+            Console.WriteLine(ZeroMatrix.ToString());
+
+            Console.WriteLine("Creating Matrix from Data");
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = i;
+            }
+            Matrix loadMatrix = new Matrix(arr, 3);
+            Console.WriteLine(loadMatrix.ToString());
+
+            Console.WriteLine("Adding Matrix");
+            Matrix AddMatrix = loadMatrix + loadMatrix;
+            Console.WriteLine(AddMatrix.ToString());
+
+            Console.WriteLine("Subtracting Matrix");
+            Matrix SubtractMatrix = AddMatrix - loadMatrix;
+            Console.WriteLine(SubtractMatrix.ToString());
+
+            Console.WriteLine("Multiplying Matrix");
+            Matrix MultiplyMatrix = SubtractMatrix * AddMatrix;
+            Console.WriteLine(MultiplyMatrix.ToString());
+
+            Console.WriteLine("Transposing Matrix");
+            Matrix TransposeMatrix = MultiplyMatrix.Transpose();
+            Console.WriteLine(TransposeMatrix.ToString());
+
+            int[] arr2 = {25,5,1,64,8,1,144,12,1};
+
+            Matrix pre = new Matrix(arr2, 3);
+            Console.WriteLine("Decomposing Matrix");
+            LUMatrix lu = new LUMatrix(pre);
+            Console.WriteLine("Upper Matrix");
+            Matrix upper = lu.GetUpper();
+            Console.WriteLine(upper.ToString());
+            Console.WriteLine("Lower Matrix");
+            Matrix lower = lu.GetLower();
+            Console.WriteLine(lower.ToString());
+
+            Console.Read();
+        }
+    }
+
+
+}
---
+++ Sharpe/Matrix/LUMatrix.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharpe.Numbers;
+
+namespace Sharpe.Matrix
+{
+    /// <summary>
+    /// Lower Upper Matrix class for decomposition
+    /// </summary>
+    public class LUMatrix
+    {
+        private Tuple<Matrix, Matrix> LU;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m"></param>
+        public LUMatrix(Matrix m)
+        {
+            Matrix U = new Matrix(m.NumRows, m.NumCols);
+            Console.WriteLine(m.ToString());
+            //Begin a Decomposition.
+            Queue<Number> Lower = new Queue<Number>();
+
+            //Copy the first row, it remains unchanged
+            U[0] = m[0];
+            for (int i = 1; i < m.NumRows; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    //Get the number we're supposed to scale by
+                    Number scalar = m[i][j] / m[i - 1][j];
+                     //Enqueue the number for later use
+                    Lower.Enqueue(scalar);
+                    //Perform the scale  
+                    Number[] scaled = Matrix.Scale(m[i - 1], scalar);
+                    //Subtract
+                    U[i] = Matrix.RowSubtract(m[i], scaled);
+                }
+            }
+
+            for (int j = 0; j < m.NumCols; j++)
+            {
+                //Get the number we're supposed to scale by
+                Number scalar = m[m.NumRows - 1][j] / m[m.NumRows - 2][j];
+                //Enqueue the number for later use
+                Lower.Enqueue(scalar);
+                //Perform the scale  
+                Number[] scaled = Matrix.Scale(m[m.NumRows - 2], scalar);
+                //Subtract
+                U[m.NumRows - 1] = Matrix.RowSubtract(m[m.NumRows - 1], scaled);
+            }
+
+            Console.Read();
+            Console.WriteLine(U.ToString());
+
+            Matrix L = new IdentityMatrix(m.NumRows);
+            //int count = 1;
+            //while (Lower.Count > 0)
+            //{
+            //    L[count+1][count] = Lower.Dequeue();
+            //    count++;
+            //}
+
+            LU = Tuple.Create<Matrix, Matrix>(L, U);
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Matrix GetUpper()
+        {
+            return LU.Item2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Matrix GetLower()
+        {
+            return LU.Item1;
+        }
+    }
+}
